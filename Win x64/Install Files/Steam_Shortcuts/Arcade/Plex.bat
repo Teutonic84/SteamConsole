51b77a0bc4186fdc2972d2776377dc4c8908a86d
Teutonic84 <soccrguy23@mchsi.com>
2016-04-05 15:14:47 -0500

    Added New Website Links to Apps & Gaming section
    Added Hulu, Showtime and Tru TV website links to the Apps & Gaming
    section in Steam.

--- Win x64/Install Files/Steam_Shortcuts/Arcade/Plex.bat
+++ Win x64/Install Files/Steam_Shortcuts/Arcade/Plex.bat
@@ -11,7 +11,7 @@ if not exist "C:\Program Files (x86)" (
 )
 
 ::start "" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile"
-"C:\Program Files (x86)\PlexMediaPlayer\PlexMediaPlayer.exe"
+"C:\Program Files\PlexMediaPlayer\PlexMediaPlayer.exe"
 
 ::"%dirpath%\Tools\Xpadder\xpadder.exe" /C
 ::start "" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile"
---
+++ Win x64/Install Files/Steam_Shortcuts/Arcade/Tru TV.bat
@@ -0,0 +1,19 @@
+echo off
+
+cd ..\..
+set dirpath=%cd%
+
+if exist "C:\Program Files (x86)" (
+    set "programfiles=C:\Program Files (x86)"
+)
+if not exist "C:\Program Files (x86)" (
+    set "programfiles=C:\Program Files"
+)
+
+start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile"
+"%programfiles%\Google\Chrome\Application\chrome.exe" --kiosk "http://www.trutv.com/"
+
+"%dirpath%\Tools\Xpadder\xpadder.exe" /C
+start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile"
+
+exit
