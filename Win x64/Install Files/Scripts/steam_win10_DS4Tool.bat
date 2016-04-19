0a752e4533fe4583f4de94a0ae2fbaaef0d1634e
Teutonic84 <soccrguy23@mchsi.com>
2016-04-18 19:24:42 -0500

    Updated SteamConsole Script
    Updated steam_win10_DS4Tool.bat

--- Win x64/Install Files/Scripts/steam_win10_DS4Tool.bat
+++ Win x64/Install Files/Scripts/steam_win10_DS4Tool.bat
@@ -1,26 +1,32 @@
 echo off
-
 cd ..
 set dirpath=%cd%
-set "programfiles=C:\Program Files (x86)"
-cls
+if exist "%dirpath%\steam_path.txt" del "%dirpath%\steam_path.txt"
 
+  taskkill /f /im "Custom Hotkeys.exe"
+  cscript.exe "%dirpath%\Scripts\steam_path_check.vbs" > "%dirpath%\steam_path.txt"
+
+:steampath
+for /F "usebackq delims=" %%i in ("%dirpath%\steam_path.txt") do set "steampath=%%i"
+del "%dirpath%\steam_path.txt"
+
+"%dirpath%\Tools\Xpadder\Xpadder.exe" /C
+cls
 ECHO.
 ECHO.
 ECHO          **       ****    *****   ***     ****  **    **    *****
 ECHO          **      **  **  **   **  ** **    **   ***   **   **   **
-ECHO          **      **  **  *******  **  **   **   ** *  **  **   
+ECHO          **      **  **  *******  **  **   **   ** *  **  **
 ECHO          **      **  **  **   **  **  **   **   **  * **  **  ****
 ECHO          ******  **  **  **   **  ** **    **   **   ***   **   **
 ECHO          ******   ****   **   **  ***     ****  **    **    *****
 ECHO.
 ECHO.
 ECHO.
-
+echo.
 start "" "%dirpath%\Tools\DS4Tool-1.2.2\ScpServer.exe"
-::start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile"
 start "" "%dirpath%\Tools\Xpadder\Custom Hotkeys.exe"
-"%dirpath%\Scripts\services.lnk"
-start "" "%programfiles%\Steam\Steam.exe"
-
+start "" "%dirpath%\Tools\Xpadder\Xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile"
+start "" "%steampath%\Steam.exe" -start steam://open/bigpicture
+if exist "C:\Program Files (x86)\Google\Drive\googledrivesync.exe" start "" "C:\Program Files (x86)\Google\Drive\googledrivesync.exe"
 exit
