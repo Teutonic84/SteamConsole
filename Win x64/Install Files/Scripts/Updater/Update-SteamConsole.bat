74b26c02e5d42d76c061a6394e67bb3252ff3cbd
Teutonic84 <soccrguy23@mchsi.com>
2016-04-28 13:53:33 -0500

    Updated Update-SteamConsole.bat File
    Cleaned up output and changed to 7zG.exe from 7za.exe to get a progress
    bar and not have a ton of output from files being extracted in the
    command window.

--- Win x64/Install Files/Scripts/Updater/Update-SteamConsole.bat
+++ Win x64/Install Files/Scripts/Updater/Update-SteamConsole.bat
@@ -7,10 +7,10 @@ cd "%dirpath%\Scripts\Updater"
 if not exist "..\..\Images" (mkdir ..\..\Images)
 if not exist "files" (mkdir Files)
 
-::taskkill /T /IM "ScpService.exe"
-taskkill /f /im "Custom Hotkeys.exe"
-taskkill /f /im "Steam.exe"
-"..\..\Tools\Xpadder\Xpadder.exe" /C
+::taskkill /T /IM "ScpService.exe" 2>NUL 1>NUL
+taskkill /f /im "Custom Hotkeys.exe" 2>NUL 1>NUL
+taskkill /f /im "Steam.exe" 2>NUL 1>NUL
+"..\..\Tools\Xpadder\Xpadder.exe" /C 2>NUL 1>NUL
 
 :steampath
 if exist "..\..\steam_path.txt" del "..\..\steam_path.txt"
@@ -24,23 +24,23 @@ del "..\..\steam_path.txt"
 ::=======================
 :updater
 cls
-wget -N --no-parent --tries=3 --html-extension --secure-protocol=tlsv1 --no-check-certificate https://github.com/Teutonic84/SteamConsole/releases/
+wget -N --no-parent --tries=3 --html-extension --secure-protocol=tlsv1 --no-check-certificate https://github.com/Teutonic84/SteamConsole/releases/ 2>NUL 1>NUL
 for /F "tokens=8 delims=/ " %%h in ('findstr /I " Updater_ " index.html') do (
     set "newversionup=%%h"
     goto nextup
 )
 set "updater=Updater Host Down Currently..."
-del /q "index.html"
+del /q "index.html" 2>NUL 1>NUL
 goto steamconsole
 
 :nextup
 cls
-del /q "index.html"
+del /q "index.html" 2>NUL 1>NUL
 set newversionup=%newversionup:Updater_v=%
 set newversionup=%newversionup:.7z"=%
 set "key=HKLM\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\SteamConsole"
 
-Reg query "%key%" /v Updater
+Reg query "%key%" /v Updater 2>NUL 1>NUL
 if %errorlevel%==1 goto update
 
 for /f "tokens=3 delims= " %%i in ('Reg query "%key%" /v Updater') do set "currentver=%%i"
@@ -48,17 +48,21 @@ if %currentver% == %newversionup% goto steamconsole
 
 :update
 cls
-ping haackerit.duckdns.org -n 1
+ping haackerit.duckdns.org -n 1 2>NUL 1>NUL
 if %errorlevel%==1 (
     set "updater=Updater Host Down Currently..."
     goto steamconsole
 )
-wget --tries=3 --ftp-user=public --ftp-password="[anthakth15" --no-check-certificate --secure-protocol=auto "ftp://haackerit.duckdns.org/Updater_v%newversionup%.7z"
-
-7za x -y -o"files\Updater_v%newversionup%" Updater_v%newversionup%.7z
+cls
+echo Downloading SteamConsole Updater v%newversionup% files...
+wget --tries=3 --ftp-user=public --ftp-password="[anthakth15" --no-check-certificate --secure-protocol=auto "ftp://haackerit.duckdns.org/Updater_v%newversionup%.7z" 2>NUL 1>NUL
+cls
+echo Extracting SteamConsole Updater v%newversionup% files...
+7zG x -y -o"files\Updater_v%newversionup%" Updater_v%newversionup%.7z
 del "Updater_v%newversionup%.7z"
-
-robocopy "Files\Updater_v%newversionup%\Updater" ..\..\..\SteamConsole\Scripts\Updater "*.*" /E /XO /MOVE
+cls
+echo Replacing old files with new ones...
+robocopy "Files\Updater_v%newversionup%\Updater" ..\..\..\SteamConsole\Scripts\Updater "*.*" /E /XO /MOVE 2>NUL 1>NUL
 
 echo Windows Registry Editor Version 5.00 >"%dirpath%\Scripts\Updater\reg_add.reg"
 echo. >>"%dirpath%\Scripts\Updater\reg_add.reg"
@@ -87,7 +91,7 @@ goto end
 ::============================
 :steamconsole
 cls
-wget -N --no-parent --tries=3 --html-extension --secure-protocol=tlsv1 --no-check-certificate https://github.com/Teutonic84/SteamConsole/releases/
+wget -N --no-parent --tries=3 --html-extension --secure-protocol=tlsv1 --no-check-certificate https://github.com/Teutonic84/SteamConsole/releases/ 2>NUL 1>NUL
 for /F "tokens=6 delims=/ " %%f in ('findstr /I " Teutonic84/SteamConsole/tree " index.html') do (
     set newversionsc="%%f
     goto nextsc
@@ -105,24 +109,28 @@ del /q "index.html"
 if %currentver% == %newversionsc% goto NOUPSC
 
 cls
-ping haackerit.duckdns.org -n 1
+ping haackerit.duckdns.org -n 1 2>NUL 1>NUL
 if %errorlevel%==1 (
     set "steamconsole=SteamConsole Host Down Currently..."
     goto cores
 )
-wget --tries=3 --ftp-user=public --ftp-password="[anthakth15" --no-check-certificate --secure-protocol=auto "ftp://haackerit.duckdns.org/SteamConsole_%newversionsc%_x64.7z"
-
-7za x -y -o"files\SteamConsole_%newversionsc%_x64" SteamConsole_%newversionsc%_x64.7z
+cls
+echo Downloading SteamConsole %newversionsc% files...
+wget --tries=3 --ftp-user=public --ftp-password="[anthakth15" --no-check-certificate --secure-protocol=auto "ftp://haackerit.duckdns.org/SteamConsole_%newversionsc%_x64.7z" 2>NUL 1>NUL
+cls
+echo Extracting SteamConsole %newversionsc% files...
+7zG x -y -o"files\SteamConsole_%newversionsc%_x64" SteamConsole_%newversionsc%_x64.7z
 del "SteamConsole_%newversionsc%_x64.7z"
+cls
+echo Replacing old files with new ones...
+mkdir "Files\SteamConsole_%newversionsc%_x64\Root" 2>NUL 1>NUL
+move /y "Files\SteamConsole_%newversionsc%_x64\Changelog.rtf" "Files\SteamConsole_%newversionsc%_x64\Root\Changelog.rtf" 2>NUL 1>NUL
+move /y "Files\SteamConsole_%newversionsc%_x64\License.rtf" "Files\SteamConsole_%newversionsc%_x64\Root\License.rtf" 2>NUL 1>NUL
+move /y "Files\SteamConsole_%newversionsc%_x64\README.txt" "Files\SteamConsole_%newversionsc%_x64\Root\README.txt" 2>NUL 1>NUL
+move /y "Files\SteamConsole_%newversionsc%_x64\SteamConsole_uninstaller.exe" "Files\SteamConsole_%newversionsc%_x64\Root\SteamConsole_uninstaller.exe" 2>NUL 1>NUL
 
-mkdir "Files\SteamConsole_%newversionsc%_x64\Root"
-move /y "Files\SteamConsole_%newversionsc%_x64\Changelog.rtf" "Files\SteamConsole_%newversionsc%_x64\Root\Changelog.rtf"
-move /y "Files\SteamConsole_%newversionsc%_x64\License.rtf" "Files\SteamConsole_%newversionsc%_x64\Root\License.rtf"
-move /y "Files\SteamConsole_%newversionsc%_x64\README.txt" "Files\SteamConsole_%newversionsc%_x64\Root\README.txt"
-move /y "Files\SteamConsole_%newversionsc%_x64\SteamConsole_uninstaller.exe" "Files\SteamConsole_%newversionsc%_x64\Root\SteamConsole_uninstaller.exe"
-
-robocopy "Files\SteamConsole_%newversionsc%_x64\Install Files" ..\..\..\SteamConsole\ "*.*" /E /XO /XD "DS4Tool-1.2.2" "DSTool-Reloaded" /MOVE
-robocopy "Files\SteamConsole_%newversionsc%_x64\Root" ..\..\..\SteamConsole\ "*.*" /E /XO /MOVE
+robocopy "Files\SteamConsole_%newversionsc%_x64\Install Files" ..\..\..\SteamConsole\ "*.*" /E /XO /XD "DS4Tool-1.2.2" "DSTool-Reloaded" /MOVE 2>NUL 1>NUL
+robocopy "Files\SteamConsole_%newversionsc%_x64\Root" ..\..\..\SteamConsole\ "*.*" /E /XO /MOVE 2>NUL 1>NUL
 
 echo Windows Registry Editor Version 5.00 >"%dirpath%\Scripts\Updater\reg_add.reg"
 echo. >>"%dirpath%\Scripts\Updater\reg_add.reg"
@@ -144,25 +152,26 @@ if exist "..\steam.bat" (
     del /q "..\steam.bat"
     copy /y "steam.bat" "..\steam.bat"
 )
-if exist "Files\SteamConsole_%newversionsc%_x64" rmdir /s /q "Files\SteamConsole_%newversionsc%_x64"
-if exist "..\..\Emulators\Updater" rmdir /s /q "..\..\Emulators\Updater"
-if exist "..\..\Emulators\Gamecube\Dolphin\Updater" rmdir /s /q "..\..\Emulators\Gamecube\Dolphin\Updater"
-if exist "..\..\Steam_Shortcuts\PC_Games" rmdir /s /q "..\..\Steam_Shortcuts\PC_Games"
-if exist "..\..\Emulators\RetroArch_x64" rename "..\..\Emulators\RetroArch_x64" RetroArch
-if exist "..\..\Steam_Grid_Images" move "..\..\Steam_Grid_Images" "..\..\Images"
-if exist "..\..\steam_path_check.vbs" del /q "..\..\steam_path_check.vbs"
-if exist "..\..\steam_path_check_x64.vbs" del /q "..\..\steam_path_check_x64.vbs"
-if exist "..\..\steam_path_check_x86.vbs" del /q "..\..\steam_path_check_x86.vbs"
-if exist "..\..\replace.vbs" del /q "..\..\replace.vbs"
-if exist "..\..\replace2.vbs" del /q "..\..\replace2.vbs"
-if exist "..\..\Shortcut.exe" del /q "..\..\Shortcut.exe"
-if exist "..\..\Scripts\Logoff.bat" del /q "..\..\Scripts\Logoff.bat"
-if exist "..\..\Scripts\ROM_Rename.bat" del /q "..\..\Scripts\ROM_Rename.bat"
-if exist "..\..\Scripts\ROM_Shortcut_Blank_Dolphin.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_Dolphin.bat"
-if exist "..\..\Scripts\ROM_Shortcut_Blank_ePSXe.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_ePSXe.bat"
-if exist "..\..\Scripts\ROM_Shortcut_Blank_PCSX2.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_PCSX2.bat"
-if exist "..\..\Scripts\Services.bat" del /q "..\..\Scripts\Services.bat"
-
+if exist "Files\SteamConsole_%newversionsc%_x64" rmdir /s /q "Files\SteamConsole_%newversionsc%_x64" 2>NUL 1>NUL
+if exist "..\..\Emulators\Updater" rmdir /s /q "..\..\Emulators\Updater" 2>NUL 1>NUL
+if exist "..\..\Emulators\Gamecube\Dolphin\Updater" rmdir /s /q "..\..\Emulators\Gamecube\Dolphin\Updater" 2>NUL 1>NUL
+if exist "..\..\Steam_Shortcuts\PC_Games" rmdir /s /q "..\..\Steam_Shortcuts\PC_Games" 2>NUL 1>NUL
+if exist "..\..\Emulators\RetroArch_x64" rename "..\..\Emulators\RetroArch_x64" RetroArch 2>NUL 1>NUL
+if exist "..\..\Steam_Grid_Images" move "..\..\Steam_Grid_Images" "..\..\Images" 2>NUL 1>NUL
+if exist "..\..\steam_path_check.vbs" del /q "..\..\steam_path_check.vbs" 2>NUL 1>NUL
+if exist "..\..\steam_path_check_x64.vbs" del /q "..\..\steam_path_check_x64.vbs" 2>NUL 1>NUL
+if exist "..\..\steam_path_check_x86.vbs" del /q "..\..\steam_path_check_x86.vbs" 2>NUL 1>NUL
+if exist "..\..\replace.vbs" del /q "..\..\replace.vbs" 2>NUL 1>NUL
+if exist "..\..\replace2.vbs" del /q "..\..\replace2.vbs" 2>NUL 1>NUL
+if exist "..\..\Shortcut.exe" del /q "..\..\Shortcut.exe" 2>NUL 1>NUL
+if exist "..\..\Scripts\Logoff.bat" del /q "..\..\Scripts\Logoff.bat" 2>NUL 1>NUL
+if exist "..\..\Scripts\ROM_Rename.bat" del /q "..\..\Scripts\ROM_Rename.bat" 2>NUL 1>NUL
+if exist "..\..\Scripts\ROM_Shortcut_Blank_Dolphin.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_Dolphin.bat" 2>NUL 1>NUL
+if exist "..\..\Scripts\ROM_Shortcut_Blank_ePSXe.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_ePSXe.bat" 2>NUL 1>NUL
+if exist "..\..\Scripts\ROM_Shortcut_Blank_PCSX2.bat" del /q "..\..\Scripts\ROM_Shortcut_Blank_PCSX2.bat" 2>NUL 1>NUL
+if exist "..\..\Scripts\Services.bat" del /q "..\..\Scripts\Services.bat" 2>NUL 1>NUL
+cls
+echo SteamConsole successfully updated to %newversionsc%...
 set "steamconsole=SteamConsole Updated to %newversionsc%..."
 goto cores
 
@@ -180,43 +189,46 @@ set "steamconsole=SteamConsole already up to date..."
 ::|  Cores Section    |
 ::=====================
 :cores
+cls
+echo Downloading latest RetroArch cores...
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/mupen64plus_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/desmume_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/genesis_plus_gx_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/mednafen_psx_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/nestopia_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/ppsspp_libretro.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/snes9x_libretro.dll.zip 2>NUL 1>NUL
+wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/vbam_libretro.dll.zip 2>NUL 1>NUL
+cls
+echo Extracting RetroArch cores...
+7zG x -y -o"files\RetroArch_Cores" "mupen64plus_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "desmume_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "genesis_plus_gx_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "mednafen_psx_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "nestopia_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "ppsspp_libretro.zip"
+7zG x -y -o"files\RetroArch_Cores" "snes9x_libretro.dll.zip"
+7zG x -y -o"files\RetroArch_Cores" "vbam_libretro.dll.zip"
 
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/mupen64plus_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/desmume_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/genesis_plus_gx_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/mednafen_psx_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/nestopia_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/ppsspp_libretro.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/snes9x_libretro.dll.zip
-wget --tries=3 http://buildbot.libretro.com/nightly/windows/x86_64/latest/vbam_libretro.dll.zip
-
-7za x -y -o"files\RetroArch_Cores" "mupen64plus_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "desmume_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "genesis_plus_gx_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "mednafen_psx_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "nestopia_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "ppsspp_libretro.zip"
-7za x -y -o"files\RetroArch_Cores" "snes9x_libretro.dll.zip"
-7za x -y -o"files\RetroArch_Cores" "vbam_libretro.dll.zip"
-
-del mupen64plus_libretro.dll.zip
-del desmume_libretro.dll.zip
-del genesis_plus_gx_libretro.dll.zip
-del mednafen_psx_libretro.dll.zip
-del nestopia_libretro.dll.zip
-del ppsspp_libretro.zip
-del snes9x_libretro.dll.zip
-del vbam_libretro.dll.zip
-
-robocopy "Files\RetroArch_Cores" ..\..\Emulators\RetroArch\cores\ /E /XO /MOVE
-if exist "files\RetroArch_Cores" rmdir /s /q "files\RetroArch_Cores"
+del mupen64plus_libretro.dll.zip 2>NUL 1>NUL
+del desmume_libretro.dll.zip 2>NUL 1>NUL
+del genesis_plus_gx_libretro.dll.zip 2>NUL 1>NUL
+del mednafen_psx_libretro.dll.zip 2>NUL 1>NUL
+del nestopia_libretro.dll.zip 2>NUL 1>NUL
+del ppsspp_libretro.zip 2>NUL 1>NUL
+del snes9x_libretro.dll.zip 2>NUL 1>NUL
+del vbam_libretro.dll.zip 2>NUL 1>NUL
+cls
+echo Replacing new files with old ones...
+robocopy "Files\RetroArch_Cores" ..\..\Emulators\RetroArch\cores\ /E /XO /MOVE 2>NUL 1>NUL
+if exist "files\RetroArch_Cores" rmdir /s /q "files\RetroArch_Cores" 2>NUL 1>NUL
 
 ::=====================
 ::|  PCSX2 Section    |
 ::=====================
 :pcsx2
 set /p version=<version_pcsx2.txt
-wget -N --tries=3 --no-parent --html-extension --secure-protocol=tlsv1 --no-check-certificate "http://buildbot.orphis.net/pcsx2/index.php"
+wget -N --tries=3 --no-parent --html-extension --secure-protocol=tlsv1 --no-check-certificate "http://buildbot.orphis.net/pcsx2/index.php" 2>NUL 1>NUL
 for /F "tokens=4 delims=><" %%a in ('findstr /I " \<v1.* " index.php.html') do (
     set "newversion=%%a"
     goto vercheck
@@ -225,19 +237,25 @@ set "pcsx2=PCSX2 Host Down Currently..."
 goto dolphin
 
 :vercheck
-del /q index.php.html
+del /q index.php.html 2>NUL 1>NUL
 cls
 if %version% == %newversion% goto NOUP
 
-wget -Q7m --tries=3 --reject css,html --mirror -p --convert-links -P files\PCSX2 "http://buildbot.orphis.net/pcsx2/index.php"
-
-7za x -y -o"files\PCSX2" "Files\PCSX2\buildbot.orphis.net\pcsx2\index.php@m=get&rev=%newversion%&platform=windows-x86"
-robocopy "Files\PCSX2\pcsx2-%newversion%-windows-x86" ..\..\Emulators\PS2\PCSX2\ /E /XO /MOVE
-copy /y portable.ini "..\..\Emulators\PS2\PCSX2\portable.ini"
+cls
+echo Downloading PCSX2 %newversion% files...
+wget -Q7m --tries=3 --reject css,html --mirror -p --convert-links -P files\PCSX2 "http://buildbot.orphis.net/pcsx2/index.php" 2>NUL 1>NUL
+cls
+echo Extracting PCSX2 %newversion% files...
+7zG x -y -o"files\PCSX2" "Files\PCSX2\buildbot.orphis.net\pcsx2\index.php@m=get&rev=%newversion%&platform=windows-x86"
+cls
+echo Replacing old files with new ones...
+robocopy "Files\PCSX2\pcsx2-%newversion%-windows-x86" ..\..\Emulators\PS2\PCSX2\ /E /XO /MOVE 2>NUL 1>NUL
+copy /y portable.ini "..\..\Emulators\PS2\PCSX2\portable.ini" 2>NUL 1>NUL
 "..\..\Emulators\PS2\PCSX2\4gb_patch.exe" ..\..\Emulators\PS2\pcsx2\pcsx2.exe
 
-if exist "files\PCSX2" rmdir /s /q "files\PCSX2"
+if exist "files\PCSX2" rmdir /s /q "files\PCSX2" 2>NUL 1>NUL
 @echo %newversion%>version_pcsx2.txt
+echo PCSX2 successfully updated to %newversion%...
 set "pcsx2=PCSX2 Updated to %newversion%..."
 goto dolphin
 
@@ -255,10 +273,8 @@ set "pcsx2=PCSX2 already up to date..."
 ::|  Dolphin Section    |
 ::=======================
 :dolphin
-echo Dolphin Updater 1.0 by Drakekilla
-
-wget -N --tries=3 --no-parent --html-extension --secure-protocol=tlsv1 --no-check-certificate https://dolphin-emu.org/download/
-
+cls
+wget -N --tries=3 --no-parent --html-extension --secure-protocol=tlsv1 --no-check-certificate https://dolphin-emu.org/download/ 2>NUL 1>NUL
 for /F "tokens=2 delims=)(" %%a in ('findstr /I " \<(.* " index.html') do (
     set "newversion2=%%a"
     goto next
@@ -268,20 +284,26 @@ goto retroarch
 
 :next
 set /p version2=<version_dolphin.txt
-del /q "index.html"
+del /q "index.html" 2>NUL 1>NUL
 
 if %version2% == %newversion2% goto NOUP2
 
 set link=http://dl.dolphin-emu.org/builds/dolphin-master-%newversion2%-x64.7z
 cls
-wget --tries=3 --no-check-certificate %link%
-7za x -y -o"files\" dolphin-master-%newversion2%-x64.7z
-::7za x dolphin-master-%newversion2%-x64.7z
-del dolphin-master-%newversion2%-x64.7z
-
+echo Downloading Dolphin %newversion2% files...
+wget --tries=3 --no-check-certificate %link% 2>NUL 1>NUL
+cls
+echo Extracting Dolphin %newversion2% files...
+7zG x -y -o"files\" dolphin-master-%newversion2%-x64.7z
+del dolphin-master-%newversion2%-x64.7z 2>NUL 1>NUL
+cls
+echo Replacing old files with new ones...
 @echo %newversion2%>version_dolphin.txt
-robocopy Files\Dolphin-x64 ..\..\Emulators\Gamecube\Dolphin\ /E /XO /MOVE
-if exist "Dolphin-x64" rmdir /s /q Dolphin-x64
+robocopy Files\Dolphin-x64 ..\..\Emulators\Gamecube\Dolphin\ /E /XO /MOVE 2>NUL 1>NUL
+
+if exist "Dolphin-x64" rmdir /s /q Dolphin-x64 2>NUL 1>NUL
+cls
+echo Dolphin successfully updated to %newversion2%...
 set "dolphin=Dolphin Updated to %newversion2%..."
 goto retroarch
 
@@ -309,20 +331,24 @@ echo %datef%>date.txt
 
 set link=http://buildbot.libretro.com/nightly/win-x86_64/%datef%_RetroArch.7z
 set link2=http://buildbot.libretro.com/nightly/windows/x86_64/redist-x86_64.7z
-wget --tries=3 --no-check-certificate %link%
+wget --tries=3 --no-check-certificate %link% 2>NUL 1>NUL
 if %errorlevel%==1 (
     set "retroarch=RetroArch No nightly build for today. Try again tomorrow."
     goto complete
 )
-wget --tries=3 --no-check-certificate %link2%
-
-:next2
-7za x -y -o"files\RetroArch" %datef%_RetroArch.7z
-7za x -y -o"files\RetroArch-redist" redist-x86_64.7z
-del /q %datef%_RetroArch.7z
-del /q redist-x86_64.7z
-robocopy Files\RetroArch ..\..\Emulators\RetroArch\ /E /XO /MOVE
-robocopy Files\RetroArch-redist ..\..\Emulators\RetroArch\ /E /XO /MOVE
+cls
+echo Downloading RetroArch Nightly %datef%...
+wget --tries=3 --no-check-certificate %link2% 2>NUL 1>NUL
+cls
+echo Extracting RetroArch Nightly %datef%...
+7zG x -y -o"files\RetroArch" %datef%_RetroArch.7z
+7zG x -y -o"files\RetroArch-redist" redist-x86_64.7z
+del /q %datef%_RetroArch.7z 2>NUL 1>NUL
+del /q redist-x86_64.7z 2>NUL 1>NUL
+cls
+echo Replacing old files with new ones...
+robocopy Files\RetroArch ..\..\Emulators\RetroArch\ /E /XO /MOVE 2>NUL 1>NUL
+robocopy Files\RetroArch-redist ..\..\Emulators\RetroArch\ /E /XO /MOVE 2>NUL 1>NUL
 set "retroarch=RetroArch updated to %datef%..."
 
 :complete
