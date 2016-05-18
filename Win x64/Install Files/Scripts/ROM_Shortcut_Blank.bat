c5ddd1eaa11e4781d4e4f5f596e02d84c67f6b95
Teutonic84 <soccrguy23@mchsi.com>
2016-05-18 15:01:09 -0500

    Minor Bug Fix - RetroArch Xpadder Profile

--- Win x64/Install Files/Scripts/ROM_Shortcut_Blank.bat
+++ Win x64/Install Files/Scripts/ROM_Shortcut_Blank.bat
@@ -3,7 +3,7 @@ echo off
 cd ..\..
 set "dirpath=%cd%"
 
-::start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\CONSOLE_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\CONSOLE_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\CONSOLE_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\CONSOLE_Xbox360.xpadderprofile"
+start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\RetroArch.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\RetroArch.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\RetroArch.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\RetroArch.xpadderprofile"
 copy /y "%dirpath%\Docs\Controller Maps\360 Controller - Retroarch.rtf" "%dirpath%\Docs\Controller Maps\360 Controller.rtf"
 "%dirpath%\Emulators\RetroArch\retroarch.exe" --config "%dirpath%\Emulators\RetroArch\configs\CONFIGFILE_libretro.cfg" -L "%dirpath%\Emulators\RetroArch\cores\CONFIGFILE_libretro.dll" "%dirpath%\Emulators\ROMS\CONGEN\ROMFILE" -f
 
