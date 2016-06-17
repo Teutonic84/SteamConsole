24e6a6df2349de1a4dcb323fa70b5cc8e446eea4
Teutonic84 <soccrguy23@mchsi.com>
2016-06-17 12:07:48 -0500

    Updated Apps

--- Win x64/Install Files/Steam_Shortcuts/Apps/Fox Sports Go.bat
+++ Win x64/Install Files/Steam_Shortcuts/Apps/Fox Sports Go.bat
@@ -6,6 +6,11 @@ set "dirpath=%cd%"
 start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Chrome_Xbox360.xpadderprofile"
 start foxsportsgo:
 
+:check
+ping localhost -n 3 2>NUL 1>NUL
+tasklist /FI "IMAGENAME eq foxsportsgo.exe" 2>NUL | find /I /N "foxsportsgo.exe">NUL
+if "%ERRORLEVEL%"=="0" goto check
+
 ::"%dirpath%\Tools\Xpadder\xpadder.exe" /C
 ::start "" "%dirpath%\Tools\Xpadder\xpadder.exe" /M "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile" "%dirpath%\Tools\Xpadder\Controller-Profiles\Steam_Xbox360.xpadderprofile"
 
