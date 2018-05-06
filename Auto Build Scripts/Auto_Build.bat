4fe630be40e8b021d17038d4f78f13598ce1ea88
Teutonic84 <soccrguy23@mchsi.com>
2018-05-06 00:55:12 -0500

    Updated Auto Build Script

--- Auto Build Scripts/Auto_Build.bat
+++ Auto Build Scripts/Auto_Build.bat
@@ -1,29 +1,44 @@
-echo off
-cls
+ECHO off
+CLS
 
-set /P updaterv=Please type version of updater script (ex: 1.2.3): %=%
+SET /P updaterv=Please type version of updater script (ex: 1.2.3): %=%
+SET /P scversion=Please type version of SteamConsole (ex: 1.2.3): %=%
 
-echo Copying Win x64 folder to new location before building...
-::robocopy "..\Win x64" "C:\Users\User\Desktop\Downloads\Win x64" /E /Z
-::cls
-echo.
-echo Copying done...
-ping localhost -n 2 2<NUL 1<NUL
+ECHO Copying Win x64 folder to new location before building...
+::robocopy "..\Win x64" "%userprofile%\Desktop\Downloads\Win x64" /E /Z
+ECHO.
+ECHO Done
+PING localhost -n 2 >NUL
 
-echo.
-echo Zipping folder "Updater"
-7zg a -y "C:\Users\User\Desktop\Downloads\Updater_v%updaterv%.7z" "C:\Users\User\Desktop\Downloads\Win x64\Install Files\Scripts\Updater"
+ECHO.
+ECHO Starting SteamConsole Updater script build.
+ECHO Zipping folder "Updater"...
+7zg a -y "%userprofile%\Desktop\Downloads\Updater_v%updaterv%.7z" "%userprofile%\Desktop\Downloads\Win x64\Install Files\Scripts\Updater"
+ECHO.
+ECHO Done
+PING localhost -n 2 >NUL
 
-echo.
-echo Zipping of Updater_v%updaterv%.7z completed successfully...
-ping localhost -n 2 2<NUL 1<NUL
+ECHO.
+ECHO Moving Updater_v%updaterv%.7z to SteamConsole FTP folder...
+MOVE /y "%userprofile%\Desktop\Downloads\Updater_v%updaterv%.7z" "C:\ftp_root\steamconsole\Updater_v%updaterv%.7z"
+ECHO.
+ECHO Done
+ECHO SteamConsole Updater script build complete.
+PING localhost -n 2 >NUL
 
-echo.
-echo Moving Updater_v%updaterv%.7z to SteamConsole FTP folder...
-move /y "C:\Users\User\Desktop\Downloads\Updater_v%updaterv%.7z" "C:\ftp_root\steamconsole\Updater_v%updaterv%.7z"
+ECHO.
+ECHO Starting SteamConsole main script build.
+7zg a -y "%userprofile%\Desktop\Downloads\Win x64\SteamConsole.7z" "%userprofile%\Desktop\Downloads\Win x64\Install Files\*.*"
+ECHO.
+ECHO Done
+PING localhost -n 2 >NUL
 
-echo.
-echo Moving completed successfully...
-ping localhost -n 2 2<NUL 1<NUL
+ECHO Removing Install Files folder to prep for NSIS installer build.
+RMDIR /S /Q "%userprofile%\Desktop\Downloads\Win x64\Install Files"
+ECHO Done
+PING localhost -n 2 >NUL
 
-pause
+ECHO Edit the files "Changelog.rtf" and "SteamConsole_Setup.nsi" with SteamConsole version number %scversion% before continuing.
+PAUSE
+
+PAUSE
