a664f6db5abd63f9cd9b1787c0da3e336fda9d00
Teutonic84 <soccrguy23@mchsi.com>
2019-02-28 22:39:59 -0600

    Removed chrome.bat and replaced with Web Browser.bat
    Removed chrome.bat to replace with "Web Browser.bat". This is so that users can set if they want chrome or firefox to be used as their browser in steam.

---
+++ Test Build/Install Files/Emulators/ROMS/Apps/Web Browser.bat
@@ -0,0 +1,37 @@
+ECHO off
+
+cd "%~dp0%"
+cd ..\..\..
+SET "dirpath=%cd%"
+SET "key64=HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+SET "key32=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+
+
+TASKKILL /im "Custom Hotkeys.exe"
+COPY /y "%dirpath%\Tools\Controller Maps\Controller Map - Chrome.JPG" "%dirpath%\Tools\Controller Maps\Controller Map.JPG"
+START "" "%dirpath%\Tools\Custom Hotkeys.exe"
+
+:app_path
+FOR /f "usebackq tokens=*" %%a IN ("D:\GitHub\SteamConsole\Test Build\Install Files\Config\general_settings.ini") DO (
+    FOR /f "tokens=1,2 delims==" %%b IN ('ECHO %%a') DO (
+        IF "%%c"=="enabled" (
+             IF "%%b"=="Mozilla Firefox" SET "browser=%%b" && SET "exefile=firefox.exe"
+             IF "%%b"=="Google Chrome" SET "browser=%%b" && SET "exefile=chrome.exe"
+        )
+    )
+)
+IF "%browser%"=="Mozilla Firefox" SET "key=%key32%"
+IF NOT "%browser%"=="Mozilla Firefox" SET "key=%key64%"
+FOR /f "skip=1 tokens=*" %%a IN ('Reg query "%key%" /s /f "%browser%"') DO if not defined line set "line=%%a" 2>NUL 1>NUL
+FOR /f "tokens=* delims= " %%c IN ('Reg query "%line%" /v InstallLocation') DO set "instloc=%%c" 2>NUL 1>NUL
+SET instloc=%instloc:InstallLocation    REG_SZ    =%
+SET instloc=%instloc:"=%
+
+"%instloc%\%exefile%"
+
+TASKKILL /im "Custom Hotkeys.exe"
+COPY /y "%dirpath%\Tools\Controller Maps\Controller Map - Steam.JPG" "%dirpath%\Tools\Controller Maps\Controller Map.JPG"
+START "" "%dirpath%\Tools\Custom Hotkeys.exe"
+
+:end
+EXIT
