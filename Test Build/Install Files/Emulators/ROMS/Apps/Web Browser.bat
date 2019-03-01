131a60523597389b132ecf631383cdfb75070f59
Teutonic84 <soccrguy23@mchsi.com>
2019-02-28 23:35:41 -0600

    Fixed issue with web browser.bat not loading properly

--- Test Build/Install Files/Emulators/ROMS/Apps/Web Browser.bat
+++ Test Build/Install Files/Emulators/ROMS/Apps/Web Browser.bat
@@ -6,13 +6,12 @@ SET "dirpath=%cd%"
 SET "key64=HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
 SET "key32=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 
-
 TASKKILL /im "Custom Hotkeys.exe"
 COPY /y "%dirpath%\Tools\Controller Maps\Controller Map - Chrome.JPG" "%dirpath%\Tools\Controller Maps\Controller Map.JPG"
 START "" "%dirpath%\Tools\Custom Hotkeys.exe"
 
 :app_path
-FOR /f "usebackq tokens=*" %%a IN ("D:\GitHub\SteamConsole\Test Build\Install Files\Config\general_settings.ini") DO (
+FOR /f "usebackq tokens=*" %%a IN ("%dirpath%\Config\general_settings.ini") DO (
     FOR /f "tokens=1,2 delims==" %%b IN ('ECHO %%a') DO (
         IF "%%c"=="enabled" (
              IF "%%b"=="Mozilla Firefox" SET "browser=%%b" && SET "exefile=firefox.exe"
@@ -22,8 +21,8 @@ FOR /f "usebackq tokens=*" %%a IN ("D:\GitHub\SteamConsole\Test Build\Install Fi
 )
 IF "%browser%"=="Mozilla Firefox" SET "key=%key32%"
 IF NOT "%browser%"=="Mozilla Firefox" SET "key=%key64%"
-FOR /f "skip=1 tokens=*" %%a IN ('Reg query "%key%" /s /f "%browser%"') DO if not defined line set "line=%%a" 2>NUL 1>NUL
-FOR /f "tokens=* delims= " %%c IN ('Reg query "%line%" /v InstallLocation') DO set "instloc=%%c" 2>NUL 1>NUL
+FOR /f "skip=1 tokens=*" %%d IN ('Reg query "%key%" /s /f "%browser%" /reg:64') DO if not defined line set "line=%%d"
+FOR /f "tokens=* delims= " %%e IN ('Reg query "%line%" /v InstallLocation /reg:64') DO set "instloc=%%e" 2>NUL 1>NUL
 SET instloc=%instloc:InstallLocation    REG_SZ    =%
 SET instloc=%instloc:"=%
 
